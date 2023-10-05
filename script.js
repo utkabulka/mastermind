b74f55d5f177386545a9c09e115a23ed15e4cd59
Roman Zubin <utkabulka@gmail.com>
2023-10-05 21:10:59 +0400

    placeholder for guesses

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 const numberInput = document.querySelector(".number-input");
 const guessButton = document.querySelector(".guess-button")
 
+const numberLength = 4;
+
 let generatedNumber;
 
+numberInput.maxlength = numberLength;
 generateNumber();
 
 guessButton.onclick = () => {
     let input = numberInput.value;
-    if (input.length != 4) {
+    if (input.length != numberLength) {
         console.log("Invalid length of the input!");
     }
     else {
-        numberInput.value = "";
+        let softMatches = 0;
+        let hardMatches = 0;
+        for (let i = 0; i < numberLength; i++) {
+            if (generatedNumber.charAt(i) == input.charAt(i)) {
+                hardMatches++;
+            }
+            else if (generatedNumber.includes(`${input.charAt(i)}`)) {
+                softMatches++;
+            }
+        }
+        console.log(`Guess ${input} against ${generatedNumber}; SM: ${softMatches}; HM: ${hardMatches}`);
+        // numberInput.value = "";
     }
 }
 
 function generateNumber() {
     generatedNumber = "";
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < numberLength; i++) {
         let digit = Math.floor(Math.random() * 10);
         if (i > 0) {
             while (true) {
